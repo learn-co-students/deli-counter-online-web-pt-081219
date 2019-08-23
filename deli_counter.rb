6e0146dc2e16799ae117926f80f3197804e349c8
Jeffrey Brown <brownjs03@gmail.com>
2019-08-23 04:01:03 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,22 +5,17 @@ def line(array)
     current_line = "The line is currently:"
     num = 1 
     array.each do |name|
-     customers = " #{num}. #{name}"
+     customer = " #{num}. #{name}"
      num += 1 
-     current_line << customers
+     current_line << customer
    end
      puts current_line
  end
 end
 
 def take_a_number(array, new_person)
-  if array.length == 0
-    array << new_person
-  puts "Welcome, #{new_person}. You are number 1 in line."
-else 
   array << new_person
   puts "Welcome, #{new_person}. You are number #{array.length} in line."
-end
 end
 
 def now_serving(array)
